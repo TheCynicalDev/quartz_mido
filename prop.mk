26fc9c4324193248358ebc14c7aae9ffccbcf1a6
Murat Kozan <muratkozan350@gmail.com>
2019-08-04 19:05:17 +0300

    mido: Add some properties for miracast
    * also this prop is dead for display debug.gralloc.enable_fb_ubwc

--- prop.mk
+++ prop.mk
@@ -68,7 +68,6 @@ persist.debug.coresight.config=stm-events
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.egl.hw=0 \
 debug.enable.sglscale=1 \
-debug.gralloc.enable_fb_ubwc=1 \
 debug.mdpcomp.logs=0 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
@@ -85,6 +84,7 @@ ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=0 \
 vendor.display.disable_skip_validate=1 \
+vendor.gralloc.disable_wb_ubwc=1 \
 vendor.gralloc.enable_fb_ubwc=1
 
 # DRM
@@ -190,6 +190,7 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.wfd.enable=1 \
+persist.sys.wfd.nohdcp=1 \
 persist.sys.wfd.virtual=0
 
 
