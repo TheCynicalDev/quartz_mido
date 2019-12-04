db2b9d3566aa30d58149577b88ec31c11d34680a
dracarys18 <karthihegde010@gmail.com>
2019-12-04 13:19:54 +0000

    mido: Enable frameworks boost

--- prop.mk
+++ prop.mk
@@ -200,3 +200,16 @@ persist.sys.wfd.virtual=0
 PRODUCT_PROPERTY_OVERRIDES += \
    persist.dbg.volte_avail_ovr=1 \
    persist.dbg.vt_avail_ovr=1
+
+#framework boost
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.at_library=true \
+    vendor.iop.enable_uxe=1 \
+    vendor.perf.iop_v3.enable=true \
+    vendor.perf.iop_v3.enable.debug=false \
+    vendor.enable.prefetch=false \
+    vendor.iop.enable_prefetch_ofr=false \
+    vendor.perf.gestureflingboost.enable=true \
+    vendor.perf.workloadclassifier.enable=true
+
+
